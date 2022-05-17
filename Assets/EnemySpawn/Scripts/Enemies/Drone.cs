a3322d448fe997c6ceedd121b4f695d1faa155d8
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-17 22:47:44 +0800

    Initial drone behavior implemented

--- Assets/EnemySpawn/Scripts/Enemies/Drone.cs
+++ Assets/EnemySpawn/Scripts/Enemies/Drone.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Drone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Properties")]
+    [SerializeField] float movementSpeed = 1f;
+    private Rigidbody _rigidBody;
+    [Header("Player Reference")]
+    [SerializeField] private Transform _playerTransform;
+
+    private void Awake()
     {
-        
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Reset()
     {
-        
+        movementSpeed = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        // Do not do anything if player is not yet found.
+        if (_playerTransform == null) return;
+
+        AttackPlayer();
+    }
+
+    /// <summary>
+    /// Looks for the player and tries to hit it.
+    /// </summary>
+    private void AttackPlayer()
+    {
+        Vector3 direction = _playerTransform.position - _rigidBody.transform.position;
+        _rigidBody.MovePosition(_rigidBody.transform.position + direction * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
