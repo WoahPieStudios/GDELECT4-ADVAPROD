a4320c35a4b63028daa373ef439573eb31a12793
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-17 22:59:13 +0800

    added namespaces to script

--- Assets/EnemySpawn/Scripts/Enemies/Drone.cs
+++ Assets/EnemySpawn/Scripts/Enemies/Drone.cs
@@ -1,48 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Drone : MonoBehaviour
+namespace EnemySpawn.Scripts.Enemies
 {
-
-    [Header("Properties")]
-    [SerializeField] float movementSpeed = 1f;
-    private Rigidbody _rigidBody;
-    [Header("Player Reference")]
-    [SerializeField] private Transform _playerTransform;
-
-    private void Awake()
+    public class Drone : MonoBehaviour
     {
-        _rigidBody = GetComponent<Rigidbody>();
-    }
 
-    private void Reset()
-    {
-        movementSpeed = 1f;
-    }
+        [Header("Properties")]
+        [SerializeField] float movementSpeed = 1f;
+        private Rigidbody _rigidBody;
+        [Header("Player Reference")]
+        [SerializeField] private Transform _playerTransform;
 
-    private void FixedUpdate()
-    {
-        // Do not do anything if player is not yet found.
-        if (_playerTransform == null) return;
-
-        AttackPlayer();
-    }
-
-    /// <summary>
-    /// Looks for the player and tries to hit it.
-    /// </summary>
-    private void AttackPlayer()
-    {
-        Vector3 direction = _playerTransform.position - _rigidBody.transform.position;
-        _rigidBody.MovePosition(_rigidBody.transform.position + direction * movementSpeed * Time.fixedDeltaTime);
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.collider.CompareTag("Player"))
+        private void Awake()
         {
-            Destroy(gameObject);
+            _rigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void Reset()
+        {
+            movementSpeed = 1f;
+        }
+
+        private void FixedUpdate()
+        {
+            // Do not do anything if player is not yet found.
+            if (_playerTransform == null) return;
+
+            AttackPlayer();
+        }
+
+        /// <summary>
+        /// Looks for the player and tries to hit it.
+        /// </summary>
+        private void AttackPlayer()
+        {
+            Vector3 direction = _playerTransform.position - _rigidBody.transform.position;
+            _rigidBody.MovePosition(_rigidBody.transform.position + direction * movementSpeed * Time.fixedDeltaTime);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.collider.CompareTag("Player"))
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
