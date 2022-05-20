c756cb2af94a4517d1d4b12f8a96554b29939637
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-20 22:17:49 +0800

    Set states for drone upon finding player

--- Assets/EnemySpawn/Scripts/Enemies/Drone.cs
+++ Assets/EnemySpawn/Scripts/Enemies/Drone.cs
@@ -5,12 +5,18 @@ namespace EnemySpawn.Scripts.Enemies
 {
     public class Drone : MonoBehaviour
     {
+        [Header("Debug")]
+        [SerializeField] bool isStandalone;
 
         [Header("Properties")]
         [SerializeField] float movementSpeed;
+        [SerializeField] LayerMask objectsToDetect;
         private Rigidbody _rigidBody;
-        
+        private Transform _transform;
+        private bool _isLookingForPlayer;
+
         [Header("Player Reference")]
+        [SerializeField] float attackDistance;
         private Transform _playerTransform;
 
         [Header("Drone Pool Reference")]
@@ -20,11 +26,19 @@ namespace EnemySpawn.Scripts.Enemies
         private void Awake()
         {
             _rigidBody = GetComponent<Rigidbody>();
+            _transform = transform;
+
+            if (isStandalone)
+            {
+                _playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
+                _isLookingForPlayer = true;
+            }
         }
 
         private void Reset()
         {
             movementSpeed = 1f;
+            attackDistance = 1f;
         }
 
         private void FixedUpdate()
@@ -32,17 +46,34 @@ namespace EnemySpawn.Scripts.Enemies
             // Do not do anything if player is not yet found.
             if (_playerTransform == null) return;
 
-            AttackPlayer();
+            var collisions = Physics.OverlapSphere(transform.position, attackDistance, objectsToDetect);
+            foreach (var collision in collisions)
+            {
+                // drone looks for player if it has not "seen" the player yet.
+                _isLookingForPlayer = !collision.CompareTag("Player");
+            }
+
+            if (_isLookingForPlayer) { LookForPlayer(); }
+            else { AttackPlayer(); }
         }
 
         /// <summary>
-        /// Looks for the player and tries to hit it.
+        /// Searches for the player in the scene.
         /// </summary>
-        private void AttackPlayer()
+        private void LookForPlayer()
         {
+            print("Looking for Player");
             var position = _rigidBody.transform.position;
             var direction = _playerTransform.position - position;
             _rigidBody.MovePosition(position + direction.normalized * movementSpeed * Time.fixedDeltaTime);
+        }
+
+        /// <summary>
+        /// Attack behavior of the drone towards the player.
+        /// </summary>
+        private void AttackPlayer()
+        {
+            print("Attacking Player");
         }
 
         /// <summary>
@@ -51,14 +82,29 @@ namespace EnemySpawn.Scripts.Enemies
         /// <param name="playerTransform">The transform of the player.</param>
         public void SetPlayerTransform(Transform playerTransform) => _playerTransform = playerTransform;
 
-        public void SetPool(ObjectPool<Drone> dronePool) => _dronePool = dronePool; 
-        
+        /// <summary>
+        /// Sets a reference to which pool this drone belongs to.
+        /// </summary>
+        /// <param name="dronePool">The pool where the drone came from.</param>
+        public void SetPool(ObjectPool<Drone> dronePool) => _dronePool = dronePool;
+
+        /// <summary>
+        /// Sets whether the drone should be looking for the player.
+        /// </summary>
+        /// <param name="isLookingForPlayer">The state of the drone.</param>
+        public void SetPlayerLookState(bool isLookingForPlayer) => _isLookingForPlayer = isLookingForPlayer;
+
         private void OnCollisionEnter(Collision other)
         {
             if (other.collider.CompareTag("Player"))
             {
-               _dronePool.Release(this);
+                if (isStandalone) Destroy(gameObject);
+                else { _dronePool.Release(this); }
             }
+        }
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, attackDistance);
         }
     }
 }
--- Assets/EnemySpawn/Scripts/Spawners/DroneSpawner.cs
+++ Assets/EnemySpawn/Scripts/Spawners/DroneSpawner.cs
@@ -19,7 +19,7 @@ namespace EnemySpawn.Scripts.Spawners
         [SerializeField] private float spawnInterval;
         [SerializeField] private float spawnRadius;
 
-        [Header("Player Reference")] 
+        [Header("Player Reference")]
         private Transform _playerTransform;
 
         private void Reset()
@@ -38,12 +38,12 @@ namespace EnemySpawn.Scripts.Spawners
         {
             StartCoroutine(SpawnDrones());
         }
-        
+
         /// <summary>
         /// Sets the <see cref="_playerTransform"/> by looking for objects with the tag <c>Player</c>.
         /// </summary>
         private void FindPlayerTransform() => _playerTransform = GameObject.FindGameObjectWithTag("Player")?.transform;
-        
+
         /// <summary>
         /// Gets a <see cref="Drone"/> from the <see cref="dronePool"/> and sets its position within the <see cref="spawnPoint"/> and <see cref="spawnRadius"/>.
         /// </summary>
@@ -55,21 +55,22 @@ namespace EnemySpawn.Scripts.Spawners
                 drone.transform.position = Random.insideUnitSphere * spawnRadius + spawnPoint.position;
                 drone.SetPlayerTransform(_playerTransform);
                 drone.SetPool(dronePool.Pool);
+                drone.SetPlayerLookState(true);
                 yield return new WaitForSeconds(spawnInterval);
             }
         }
-        
+
         /// <summary>
         /// Flips the current state of <see cref="isSpawning"/>.
         /// </summary>
         public void ToggleSpawn() => isSpawning = !isSpawning;
-        
+
         /// <summary>
         /// Sets <see cref="isSpawning"/> according to the bypass value.
         /// </summary>
         /// <param name="bypassValue">The state that will be set to <see cref="isSpawning"/></param>
         public void ToggleSpawn(bool bypassValue) => isSpawning = bypassValue;
-        
+
         private void OnDrawGizmosSelected()
         {
             if (spawnPoint == null) return;
