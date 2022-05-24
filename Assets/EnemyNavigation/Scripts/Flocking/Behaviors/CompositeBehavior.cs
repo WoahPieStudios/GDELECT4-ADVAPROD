e5e8780b8d9b4fcf89eaab8e88b04e35ebcd3627
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-24 21:26:44 +0800

    Composite Behaviors
    https://youtu.be/o9JdUYsPkzg

---
+++ Assets/EnemyNavigation/Scripts/Flocking/Behaviors/CompositeBehavior.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyNavigation.Scripts.Flocking
+{
+    [CreateAssetMenu(menuName = "Flock/Behavior/Composite")]
+    public class CompositeBehavior : FlockBehavior
+    {
+        [SerializeField] FlockBehavior[] behaviors;
+        [SerializeField] float[] weights;
+
+        public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+        {
+            // handle data mismatch
+            if (weights.Length != behaviors.Length)
+            {
+                Debug.LogError($"Data mismatch in {name}", this);
+                return Vector3.zero;
+            }
+
+            // set up move
+            Vector3 move = Vector3.zero;
+
+            // iterate through behaviors
+            for (int i = 0; i < behaviors.Length; i++)
+            {
+                Vector3 partialMove = behaviors[i].CalculateMove(agent, context, flock) * weights[i];
+
+                if (partialMove != Vector3.zero)
+                {
+                    if (partialMove.sqrMagnitude > weights[i] * weights[i])
+                    {
+                        partialMove.Normalize();
+                        partialMove *= weights[i];
+                    }
+                    move += partialMove;
+                }
+            }
+            return move;
+        }
+    }
+}
