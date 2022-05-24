ecf4f053cefa936f5d0c57a5530b5f84cd772d84
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-24 21:50:29 +0800

    Keeping agents on screen
    https://youtu.be/kmwVDSUivkg

---
+++ Assets/EnemyNavigation/Scripts/Flocking/Behaviors/StayInRadiusBehavior.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyNavigation.Scripts.Flocking.Behaviors
+{
+    [CreateAssetMenu(menuName = "Flock/Behavior/Stay in Radius")]
+    public class StayInRadiusBehavior : FlockBehavior
+    {
+        public Vector3 center;
+        public float radius = 15f;
+        
+        public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+        {
+            Vector3 centerOffset = center - agent.transform.position;
+            float t = centerOffset.magnitude / radius;
+            if (t < 0.9f)
+            {
+                return Vector2.zero;
+            }
+
+            return centerOffset * t * t;
+        }
+    }
+}
