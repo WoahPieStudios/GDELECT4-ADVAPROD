ccacdae4c09d3c69aaf76363d340aa1bfae75268
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-05-29 22:24:30 +0800

    boids now spawn

---
+++ Assets/Flocking.Revised/Scripts/Boid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Flocking.Revised.Scripts
+{
+    [RequireComponent(typeof(Collider))]
+    public class Boid : MonoBehaviour
+    {
+        [SerializeField] private float neighborRadius;
+        [SerializeField, Range(0f,1f)] private float avoidanceRange;
+
+        private BoidPool _pool;
+        private Collider _collider;
+
+        private void Reset()
+        {
+            neighborRadius = 1f;
+            avoidanceRange = 0.5f;
+
+            _collider = GetComponent<Collider>();
+        }
+
+        private void Start()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        public void Initialize(BoidPool pool, Vector3 position, Quaternion rotation)
+        {
+            _pool = pool;
+            transform.position = position;
+            transform.rotation = rotation;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.collider.CompareTag("Player")) { _pool.Pool.Release(this); }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position,neighborRadius);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, neighborRadius * avoidanceRange);
+        }
+    }
+}
---
+++ Assets/Flocking.Revised/Scripts/BoidSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flocking.Revised.Scripts
+{
+    public class BoidSpawner : MonoBehaviour
+    {
+        [Header("Debugging")] 
+        [SerializeField] private bool isRepeating;
+        
+        [Header("Spawn Settings")]
+        [SerializeField] private BoidPool pool;
+        [SerializeField] private float spawnRadius;
+        [SerializeField] private int spawnAmount;
+        [SerializeField] private float spawnRate;
+
+        private List<Boid> boids = new List<Boid>();
+
+        private void Reset()
+        {
+            pool = FindObjectOfType<BoidPool>();
+            spawnRadius = 1f;
+            spawnAmount = 10;
+            spawnRate = 1f;
+        }
+
+        private void Awake()
+        {
+            pool = FindObjectOfType<BoidPool>();
+        }
+
+        private void Start()
+        {
+            if (pool == null)
+            {
+                Debug.LogError("No pool found, please ensure that a Boid Pool has been set in the scene.");
+                return;
+            }
+
+            if (!isRepeating) { SpawnBoids(); }
+            else { InvokeRepeating(nameof(SpawnBoids), 1f, spawnRate); }
+        }
+
+        private void SpawnBoids()
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                var boid = pool.Pool.Get();
+                boid.Initialize(
+                    pool,
+                    Random.insideUnitSphere * spawnRadius + transform.position,
+                    Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward));
+                boids.Add(boid);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+    }
+}
