6ac211d23bd11dd40174f8daaa660eb4017ed4ae
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-10 21:49:37 +0800

    gun sfx now plays

--- Assets/Scripts/Interface/MainMenu.cs
+++ Assets/Scripts/Interface/MainMenu.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+   public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
