29fdaf9d5a44935b5ece0546e98f0d3a98b02121
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-10 22:09:38 +0800

    volume sliders now work

---
+++ Assets/AdditiveScenes/Scripts/ScriptableObjects/SFXVolumeChannel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace AdditiveScenes.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "SFX Volume Channel", menuName = "Channels/New SFX Volume Channel")]
+    public class SFXVolumeChannel : ScriptableObject
+    {
+        public void ChangeSFXVolume(float volume)
+        {
+            SoundManager.Instance.OnChangeSFXVolume(volume);
+        }
+    }
+}
--- Assets/Scripts/Managers and Patterns/SoundManager.cs
+++ Assets/Scripts/Managers and Patterns/SoundManager.cs
@@ -10,15 +10,30 @@ public class SoundManager : Singleton<SoundManager>
     public AudioSource musicSource, sfxSource;
 
     private event Action<AudioClip> onPlaySFX;
+    private event Action<float> onChangeBGMVolume, onChangeSFXVolume;
 
     private void OnEnable()
     {
         onPlaySFX += PlaySFX;
+        onChangeBGMVolume += ChangeBGMVolume;
+        onChangeSFXVolume += ChangeSFXVolume;
     }
 
+    private void ChangeBGMVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    
+    private void ChangeSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+    
     private void OnDisable()
     {
         onPlaySFX -= PlaySFX;
+        onChangeBGMVolume -= ChangeBGMVolume;
+        onChangeSFXVolume -= ChangeSFXVolume;
     }
 
     public void PlayAudio(AudioSource source, AudioClip clip, float volume)
@@ -43,5 +58,15 @@ public class SoundManager : Singleton<SoundManager>
     public void OnPlaySFX(AudioClip clip)
     {
         onPlaySFX?.Invoke(clip);
+    }
+
+    public void OnChangeBGMVolume(float volume)
+    {
+        onChangeBGMVolume?.Invoke(volume);
+    }
+    
+    public void OnChangeSFXVolume(float volume)
+    {
+        onChangeSFXVolume?.Invoke(volume);
     }
 }
