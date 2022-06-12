e145d48eb07f0b0c9be5068b36fdaa25d7254189
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-12 13:26:06 +0800

    audio now syncs between scenes

--- Assets/AdditiveScenes/Scripts/ScriptableObjects/BGMVolumeChannel.cs
+++ Assets/AdditiveScenes/Scripts/ScriptableObjects/BGMVolumeChannel.cs
@@ -5,9 +5,10 @@ namespace AdditiveScenes.Scripts.ScriptableObjects
     [CreateAssetMenu(fileName = "BGM Volume Channel", menuName = "Channels/Audio/New BGM Volume Channel")]
     public class BGMVolumeChannel : ScriptableObject
     {
-        public void ChangeBGMVolume(float volume)
+        public float GetVolume => SoundManager.Instance.GetBGMVolume;
+        public void SetVolume(float volume)
         {
-            SoundManager.Instance.OnChangeBGMVolume(volume);
+            SoundManager.Instance.OnSetBGMVolume(volume);
         }
     }
 }
