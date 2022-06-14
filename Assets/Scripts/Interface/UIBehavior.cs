4320343f82db6171543c7af36a6d3b034fb90817
kitendaya <kitendaya1@gmail.com>
2022-06-14 15:04:15 +0800

    [FEAT] UI ANIMATIONS
    - Imported Do Tween Package
    -I added enemy death sound.

---
+++ Assets/Scripts/Interface/UIBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIBehavior : MonoBehaviour
+{
+    public Transform HealthBar;
+    public Transform Ammo;
+    public Transform Objectives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        HealthBar.DOMoveY(50, 1).SetEase(Ease.InOutSine);
+        Ammo.DOMoveY(50, 1).SetEase(Ease.InOutSine);
+        Objectives.DOMoveY(1050, 1).SetEase(Ease.InOutSine);
+
+    }
+}
