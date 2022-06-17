eb8f1a4623ccb38e7b3c35407008cb9006bd6e2d
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-18 07:41:07 +0800

    [FIX] Drone Trails (#154)
    drone trails no longer affected by object pool

--- Assets/Spawning/Scripts/Enemies/Drone.cs
+++ Assets/Spawning/Scripts/Enemies/Drone.cs
@@ -20,6 +20,7 @@ namespace Spawning.Scripts.Enemies
 
         [Header("Properties")]
         [SerializeField] float movementSpeed;
+        [SerializeField] private TrailRenderer[] trailRenderers;
         private Rigidbody _rigidBody;
         private Transform _transform;
         private bool _isLookingForPlayer;
@@ -43,6 +44,7 @@ namespace Spawning.Scripts.Enemies
             movementSpeed = 1f;
             _damageAmount = 1f;
             attackDistance = 1f;
+            trailRenderers = GetComponentsInChildren<TrailRenderer>();
         }
 
         private void Awake()
@@ -131,6 +133,10 @@ namespace Spawning.Scripts.Enemies
         private void OnEnable()
         {
             _material.color = Color.white;
+            foreach (var trailRenderer in trailRenderers)
+            {
+                trailRenderer.Clear();
+            }
         }
 
         public void GetDestroyed(bool killedByPlayer = true)
