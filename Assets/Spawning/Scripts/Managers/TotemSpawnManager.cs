77596f6506ea41270f708de7d723a2a94a87ff49
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-21 14:16:26 +0800

    commented old script

--- Assets/Spawning/Scripts/Managers/TotemSpawnManager.cs
+++ Assets/Spawning/Scripts/Managers/TotemSpawnManager.cs
@@ -12,44 +12,48 @@ namespace Spawning.Scripts.Managers
         [SerializeField] private float initialDelay;
         [SerializeField] private float spawnInterval;
 
-        public void StartSpawning()
-        {
-            StartCoroutine(SpawnObject());
-        }
-
-        // TODO: Change to something non-recursive in the future
-        private IEnumerator SpawnObject()
-        {
-            yield return new WaitForSeconds(initialDelay);
-            while (!spawnPointManager.AllPointsTaken())
-            {
-                print($"Spawning {objectToSpawn.name}");
-                var point = LookForAvailablePoint();
-                var spawner = Instantiate(objectToSpawn, point.TakePointPosition(), Quaternion.identity, transform);
-                spawner.SpawnerPoint = point;
-                spawner.isInitialized = true;
-                yield return new WaitForSeconds(spawnInterval);
-            }
-            print("All points taken");
-            StartCoroutine(SpawnObject());
-        }
-
-        private SpawnPoint LookForAvailablePoint()
-        {
-            var spawnPoint = spawnPointManager.GetRandomPoint();
-            return !spawnPoint.IsTaken ? spawnPoint : LookForAvailablePoint();
-        }
-
-        public void ClearAllTotems()
-        {
-            StopCoroutine(SpawnObject());
-            var totems = FindObjectsOfType<DroneSpawner>();
-            if (totems.Length <= 0) return;
-            foreach (var totem in totems)
-            {
-                Destroy(totem.gameObject);
-            }
-        }
-
+        #region Old Spawner Script
+        // public void StartSpawning()
+        // {
+        //     StartCoroutine(SpawnObject());
+        // }
+        //
+        // // TODO: Change to something non-recursive in the future
+        // private IEnumerator SpawnObject()
+        // {
+        //     yield return new WaitForSeconds(initialDelay);
+        //     while (!spawnPointManager.AllPointsTaken())
+        //     {
+        //         print($"Spawning {objectToSpawn.name}");
+        //         var point = LookForAvailablePoint();
+        //         var spawner = Instantiate(objectToSpawn, point.TakePointPosition(), Quaternion.identity, transform);
+        //         spawner.SpawnerPoint = point;
+        //         spawner.isInitialized = true;
+        //         yield return new WaitForSeconds(spawnInterval);
+        //     }
+        //     print("All points taken");
+        //     StartCoroutine(SpawnObject());
+        // }
+        //
+        // private SpawnPoint LookForAvailablePoint()
+        // {
+        //     var spawnPoint = spawnPointManager.GetRandomPoint();
+        //     return !spawnPoint.IsTaken ? spawnPoint : LookForAvailablePoint();
+        // }
+        //
+        // public void ClearAllTotems()
+        // {
+        //     StopCoroutine(SpawnObject());
+        //     var totems = FindObjectsOfType<DroneSpawner>();
+        //     if (totems.Length <= 0) return;
+        //     foreach (var totem in totems)
+        //     {
+        //         Destroy(totem.gameObject);
+        //     }
+        // }
+        #endregion
+        
+        
+        
     }
 }
