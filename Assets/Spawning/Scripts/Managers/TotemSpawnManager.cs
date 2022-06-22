f537af5c9faae589b75d085ffa8d5874b6e44bca
Jolo Gutierrez <bulletproofpancake56@gmail.com>
2022-06-22 15:23:12 +0800

    totems should call spawn when they dies

--- Assets/Spawning/Scripts/Managers/TotemSpawnManager.cs
+++ Assets/Spawning/Scripts/Managers/TotemSpawnManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Spawning.Scripts.Containers;
 using Spawning.Scripts.Spawners;
@@ -11,49 +12,64 @@ namespace Spawning.Scripts.Managers
         [SerializeField] private SpawnPointManager spawnPointManager;
         [SerializeField] private float initialDelay;
         [SerializeField] private float spawnInterval;
+        private static event Action onSpawnEvent;
 
         #region Old Spawner Script
-        // public void StartSpawning()
-        // {
-        //     StartCoroutine(SpawnObject());
-        // }
-        //
-        // // TODO: Change to something non-recursive in the future
-        // private IEnumerator SpawnObject()
-        // {
-        //     yield return new WaitForSeconds(initialDelay);
-        //     while (!spawnPointManager.AllPointsTaken())
-        //     {
-        //         print($"Spawning {objectToSpawn.name}");
-        //         var point = LookForAvailablePoint();
-        //         var spawner = Instantiate(objectToSpawn, point.TakePointPosition(), Quaternion.identity, transform);
-        //         spawner.SpawnerPoint = point;
-        //         spawner.isInitialized = true;
-        //         yield return new WaitForSeconds(spawnInterval);
-        //     }
-        //     print("All points taken");
-        //     StartCoroutine(SpawnObject());
-        // }
-        //
-        // private SpawnPoint LookForAvailablePoint()
-        // {
-        //     var spawnPoint = spawnPointManager.GetRandomPoint();
-        //     return !spawnPoint.IsTaken ? spawnPoint : LookForAvailablePoint();
-        // }
-        //
-        // public void ClearAllTotems()
-        // {
-        //     StopCoroutine(SpawnObject());
-        //     var totems = FindObjectsOfType<DroneSpawner>();
-        //     if (totems.Length <= 0) return;
-        //     foreach (var totem in totems)
-        //     {
-        //         Destroy(totem.gameObject);
-        //     }
-        // }
+
+        private void OnEnable()
+        {
+            onSpawnEvent += StartSpawning;
+        }
+
+        private void OnDisable()
+        {
+            onSpawnEvent -= StartSpawning;
+        }
+
+        public static void OnSpawnEvent()
+        {
+            onSpawnEvent?.Invoke();
+        }
+
+        public void StartSpawning()
+        {
+            StopCoroutine(SpawnObject());
+            StartCoroutine(SpawnObject());
+        }
+
+        // TODO: Change to something non-recursive in the future
+        private IEnumerator SpawnObject()
+        {
+            yield return new WaitForSeconds(initialDelay);
+            while (!spawnPointManager.AllPointsTaken())
+            {
+                print($"Spawning {objectToSpawn.name}");
+                var point = LookForAvailablePoint();
+                var spawner = Instantiate(objectToSpawn, point.TakePointPosition(), Quaternion.identity, transform);
+                spawner.SpawnerPoint = point;
+                spawner.isInitialized = true;
+                yield return new WaitForSeconds(spawnInterval);
+            }
+            print("All points taken");
+            StartSpawning();
+        }
+
+        private SpawnPoint LookForAvailablePoint()
+        {
+            var spawnPoint = spawnPointManager.GetRandomPoint();
+            return !spawnPoint.IsTaken ? spawnPoint : LookForAvailablePoint();
+        }
+
+        public void ClearAllTotems()
+        {
+            StopCoroutine(SpawnObject());
+            var totems = FindObjectsOfType<DroneSpawner>();
+            if (totems.Length <= 0) return;
+            foreach (var totem in totems)
+            {
+                Destroy(totem.gameObject);
+            }
+        }
         #endregion
-        
-        
-        
     }
 }
