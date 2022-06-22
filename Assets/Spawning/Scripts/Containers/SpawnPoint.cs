3a3b5c18fc2147ee9b001ab2b35a611b74d1ed92
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-22 19:39:21 +0800

    [FIX] Totem Spawner Behavior (#179)
    * commented old script
    
    * totems should call spawn when they dies
    
    * fixed totem spawner behavior
    
    Co-authored-by: Jolo Gutierrez <bulletproofpancake56@gmail.com>

--- Assets/Spawning/Scripts/Containers/SpawnPoint.cs
+++ Assets/Spawning/Scripts/Containers/SpawnPoint.cs
@@ -6,14 +6,19 @@ namespace Spawning.Scripts.Containers
     public class SpawnPoint : MonoBehaviour
     {
         public bool IsTaken { get; private set; }
-        public float CooldownTime { get; private set; }
+        //public float CooldownTime { get; private set; }
 
-        public void Initialize(float cdTime)
+        public void Initialize()
         {
-            StopCoroutine(CooldownRoutine());
             IsTaken = false;
-            CooldownTime = cdTime;
         }
+        
+        // public void Initialize(float cdTime)
+        // {
+        //     StopCoroutine(CooldownRoutine());
+        //     IsTaken = false;
+        //     CooldownTime = cdTime;
+        // }
         
         /// <summary>
         /// Returns the position of this spawn point and sets <see cref="IsTaken"/> to true.
@@ -31,17 +36,19 @@ namespace Spawning.Scripts.Containers
         /// <remarks>Use <see cref="TakePointPosition"/> if availability is needed.</remarks>
         public Vector3 GetPointPosition() => transform.position;
 
-        public void StartCooldown()
-        {
-            StopCoroutine(CooldownRoutine());
-            StartCoroutine(CooldownRoutine());
-        }
-        
-        private IEnumerator CooldownRoutine()
-        {
-            IsTaken = true;
-            yield return new WaitForSeconds(CooldownTime);
-            IsTaken = false;
-        }
+        public void FreePointPosition() => IsTaken = false;
+
+        // public void StartCooldown()
+        // {
+        //     StopCoroutine(CooldownRoutine());
+        //     StartCoroutine(CooldownRoutine());
+        // }
+
+        // private IEnumerator CooldownRoutine()
+        // {
+        //     IsTaken = true;
+        //     yield return new WaitForSeconds(CooldownTime);
+        //     IsTaken = false;
+        // }
     }
 }
