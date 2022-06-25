d45f62579f34415a0cceaa352ac1da6826508197
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-06-25 21:47:36 +0800

    build is now split when zipped to be sent to public repo

---
+++ .github/scripts/split-zipper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Splits and zips a directory and optionally outputs it to a given folder
+# Author: bulletproofpancake
+
+# build directory : $1
+# volume size : $2
+# output directory : $3
+# output name: $4
+
+if [ ! -z "$1" ] && [ ! -z "$2" ] && [ ! -z "$4" ]
+then
+    zip -s $2'm' -r $4.zip $1
+
+    if [ ! -z "$3" ]
+    then
+        if [ -d "$3" ]
+        then
+            mv $1.z* $3'/'
+        else
+            mkdir $3
+            mv $1.z* $3'/'
+        fi
+        echo "$1.zip can be found at $3"
+    else
+        echo "$1.zip can be found at"
+        pwd
+    fi
+
+else
+    echo "Invalid parameters, be sure to run the command as"
+    echo "./split-zipper.sh \$buildDirectory \$volumeSize \$outputDirectory (optional)"
+fi
