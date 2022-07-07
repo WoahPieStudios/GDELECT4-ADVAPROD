c04ddc5630f5ac6848b69a796a90e0d56e337db2
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2022-07-07 12:59:25 +0800

    organized objects

--- Assets/AdditiveScenes/Scripts/ScriptableObjects/TutorialEventChannel.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using UnityEngine.Events;
-public abstract class TutorialEventChannel : ScriptableObject
-{
-    [SerializeField] protected UnityEvent onTutorialEvent;
-    public abstract void OnTutorialEvent();
-}
